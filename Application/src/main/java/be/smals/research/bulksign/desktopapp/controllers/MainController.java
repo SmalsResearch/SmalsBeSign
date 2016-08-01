819fcc8361ddcfd9658854043adfcb9cb9171899
cea <carlos.eavimadjessi@smals.be>
2016-08-01 11:31:53 +0200

    View Added

---
+++ Application/src/main/java/be/smals/research/bulksign/desktopapp/controllers/MainController.java
@@ -0,0 +1,12 @@
+package be.smals.research.bulksign.desktopapp.controllers;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+    @FXML
+    private Button selectFileButton;
+
+}
