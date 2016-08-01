a8613b0e9b33b845529abf4766517b4f6b30992b
cea <carlos.eavimadjessi@smals.be>
2016-08-01 12:59:25 +0200

    View Added

--- Application/src/main/java/be/smals/research/bulksign/desktopapp/Main.java
+++ Application/src/main/java/be/smals/research/bulksign/desktopapp/Main.java
@@ -1,22 +1,31 @@
 package be.smals.research.bulksign.desktopapp;
 
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 import java.util.stream.IntStream;
 
 /**
  * Created by kova on 26/07/2016.
  */
-public class Main extends Application{
+public class Main extends Application {
 
     public static void main(String[] args) {
-        printCount(10);
-        System.out.println("Hello Carlos!");
-        printCount(10);
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane root = FXMLLoader.load(getClass().getResource("be/smals/research/bulksign/desktopapp/views/main.fxml"));
+        GridPane root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
         primaryStage.setTitle("BulkSign Desktop");
 
         FileChooser fileChooser = new FileChooser();
