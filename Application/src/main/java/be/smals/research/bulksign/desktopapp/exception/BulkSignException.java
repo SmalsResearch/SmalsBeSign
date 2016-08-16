3bdcae291b72261b25431b28d01fa1d87a27689d
cea <carlos.eavimadjessi@smals.be>
2016-08-16 10:11:23 +0200

    JavaDoc - In progress Javadoc exceptions package - Done Javadoc services package - In progress

--- Application/src/main/java/be/smals/research/bulksign/desktopapp/exception/BulkSignException.java
+++ Application/src/main/java/be/smals/research/bulksign/desktopapp/exception/BulkSignException.java
@@ -1,7 +1,15 @@
 package be.smals.research.bulksign.desktopapp.exception;
 
+/**
+ * App exception class
+ */
 public class BulkSignException extends Exception{
 
+    /**
+     * Constructor
+     *
+     * @param message exception message
+     */
     public BulkSignException(String message) {
         super(message);
     }
