3a8b1c30ce24d720f8599520a4f3e14a25a11040
cea <carlos.eavimadjessi@smals.be>
2016-08-29 12:03:28 +0200

    VerifySigning service unit testing (In progress)

--- Application/src/main/java/be/smals/research/bulksign/desktopapp/test/VerifyServiceTest.java
+++ Application/src/main/java/be/smals/research/bulksign/desktopapp/test/VerifyServiceTest.java
@@ -1,9 +1,19 @@
 package be.smals.research.bulksign.desktopapp.test;
 
+import be.smals.research.bulksign.desktopapp.exception.BulkSignException;
 import be.smals.research.bulksign.desktopapp.services.VerifySigningService;
+import be.smals.research.bulksign.desktopapp.utilities.SigningOutput;
+import be.smals.research.bulksign.desktopapp.utilities.VerifySigningOutput;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.text.ParseException;
 
 /**
  * Created by cea on 29/08/2016.
@@ -11,10 +21,48 @@ import java.io.File;
 public class VerifyServiceTest {
     private final VerifySigningService verifySigningService = new VerifySigningService();
 
-    @Test public void verifySigningTest () {
-        File signedPdfFile  = new File(getClass().getClassLoader().getResource("files/signedPdf.pdf").getPath());
-        File signatureFile  = new File(getClass().getClassLoader().getResource("files/SignatureFile.pdf").getPath());
+    public VerifyServiceTest () {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+    }
+    /*
+    ---------- Normal cases : Everything is legit
+     */
+    @Test public void verifyCertificatesNormalCaseTest () throws SAXException, ParseException, IOException, NoSuchProviderException, BulkSignException, ParserConfigurationException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        File signatureFile  = new File(getClass().getClassLoader().getResource("files/SignatureFile.sig").getPath());
+        SigningOutput signature = verifySigningService.getSigningOutput(signatureFile);
+        VerifySigningOutput verifySigningOutput = verifySigningService.verifyCertificates(signature.certificateChain, new VerifySigningOutput());
 
-//        SigningOutput signature = verifySigningService.getSigningOutput()
+        Assert.assertTrue(verifySigningOutput.getCertificateResult().equals(VerifySigningOutput.VerifyResult.OK)
+                || verifySigningOutput.getCertificateResult().equals(VerifySigningOutput.VerifyResult.WARNING));
+    }
+    @Test public void verifySigningNormalCaseTest () throws SAXException, ParseException, IOException, NoSuchProviderException, BulkSignException, ParserConfigurationException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        File signedPdfFile  = new File(getClass().getClassLoader().getResource("files/signedPdf.pdf").getPath());
+        File signatureFile  = new File(getClass().getClassLoader().getResource("files/SignatureFile.sig").getPath());
+
+        SigningOutput signature = verifySigningService.getSigningOutput(signatureFile);
+        VerifySigningOutput output = verifySigningService.verifySigning(signedPdfFile, signature);
+
+        Assert.assertTrue(output.getOutputResult().equals(VerifySigningOutput.VerifyResult.OK)
+                || output.getOutputResult().equals(VerifySigningOutput.VerifyResult.WARNING));
+    }
+    /*
+    ---------- Signature defect cases :
+     */
+
+    /**
+     * Signature case 01 : Missing Signature node from signature file
+     */
+    @Test(expectedExceptions = BulkSignException.class)
+    public void signatureCase01 () throws SAXException, ParseException, IOException, NoSuchProviderException, BulkSignException, ParserConfigurationException, CertificateException {
+        File signatureFile  = new File(getClass().getClassLoader().getResource("files/SignatureFile-no-signature-node.sig").getPath());
+        verifySigningService.getSigningOutput(signatureFile);
+    }
+    @Test public void signatureCase02 () throws SAXException, ParseException, IOException, NoSuchProviderException, BulkSignException, ParserConfigurationException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        File signedPdfFile  = new File(getClass().getClassLoader().getResource("files/signedPdf.pdf").getPath());
+        File signatureFile  = new File(getClass().getClassLoader().getResource("files/SignatureFile-wrong-signature.sig").getPath());
+        SigningOutput signature = verifySigningService.getSigningOutput(signatureFile);
+        VerifySigningOutput output = verifySigningService.verifySigning(signedPdfFile, signature);
+
+        Assert.assertTrue(output.getOutputResult().equals(VerifySigningOutput.VerifyResult.FAILED));
     }
 }
